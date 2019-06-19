f7606d4498f9d05ee084f95903ef6c1649327d52
El-Hussein <Hussein.Salah8669@hotmail.com>
2019-06-19 12:38:54 +0200

    first commit

---
+++ server.py
@@ -0,0 +1,32 @@
+from bottle import route, run
+import json
+
+
+@route('/ahmed3lanamedaddo')
+def hello():
+    return json.dumps({"data": [
+        {"action": "JasperSitting", "actor": "Jasper", "obj": "chair"},
+        {"action": "JasperWalking", "actor": "Jasper", "obj": ""},
+        {"action": "PearlWalking", "actor": "Pearl", "obj": ""},
+        {"action": "JasperJump", "actor": "Jasper", "obj": ""},
+        # {"action": "PearlWalking", "actor": "Pearl", "obj": ""},
+        # {"action": "PearlWalking", "actor": "Pearl", "obj": ""},
+        # {"action": "PearlWalking", "actor": "Pearl", "obj": ""},
+        # {"action": "PearlIdle", "actor": "Pearl", "obj": ""},
+        # {"action": "PearlStandToSit", "actor": "Pearl", "obj": ""},
+        # {"action": "JasperJump", "actor": "Jasper", "obj": ""},
+        # {"action": "Jasper1StandToSit", "actor": "Jasper1", "obj": ""},
+        # {"action": "PearlIdle", "actor": "Pearl", "obj": ""},
+        # {"action": "JasperIdle", "actor": "Jasper", "obj": ""},
+        # {"action": "Jasper1Idle", "actor": "Jasper1", "obj": ""},
+       
+    ]})
+
+
+@route('/init')
+def hello():
+    # return json.dumps({"objects": ["Jasper"], "scene": ["street"]})
+    return json.dumps({"characters": ["Jasper",  "Pearl"], "objects": ["chair"], "scene": ["street"]})
+
+
+run(host='localhost', port=8080, debug=True)
